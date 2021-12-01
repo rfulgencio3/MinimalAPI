c4f340190f0cf7e0f81aebe3488e7ec2c4d43664
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-12-01 08:33:52 -0300

    Add project files.

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/teams", () =>
+{
+    var teams = new List<Team>
+    {
+        new Team("Cruzeiro Esporte Clube", "Belo Horizonte", "Brazil"),
+        new Team("São Paulo Futebol Clube", "São Paulo", "Brazil"),
+        new Team("Club Atletico Peñarol", "Montevideo", "Uruguay"),
+        new Team("Real Madrid CF", "Madrid", "Spain"),
+        new Team("Galatasaray Spor Kulübü", "Istanbul", "Turkey")
+    };
+
+    return Results.Ok(teams);
+})
+.WithName("GetTeams");
+
+app.Run();
+
+internal record Team(string name, string city, string country)
+{}
